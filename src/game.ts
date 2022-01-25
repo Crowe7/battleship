af3524a4926a844e37a8ee39c81fe503c195fefb
Crowe7 <jacobocrowe@gmail.com>
2022-01-25 06:27:33 -0600

    cpu can randomly attack player

--- src/game.ts
+++ src/game.ts
@@ -1,11 +1,19 @@
 import Gameboard from "./gameboard";
 import Player from "./players";
 
-let Computer: {board: Gameboard, player: Player, cpuPlaceShips: Function} = {
+let Computer: {board: Gameboard, player: Player, cpuPlaceShips: Function, cpuAttack: Function} = {
     board:  new Gameboard,
     player:  new Player("Computer"),
     cpuPlaceShips: () => {
         Computer.board.placeAllShipsRandomly();
+    },
+    cpuAttack: () => {
+        try {
+            let attackSpot: number = Math.floor(Math.random() * 10);
+            Human.board.takeAttack(attackSpot);
+        } catch(error) {
+            Computer.cpuAttack();
+        }
     }
 
 }
@@ -19,3 +27,5 @@ let Human: {board: Gameboard, player: Player} = {
 
 
 export {Computer, Human}
+
+
--- src/tests/game.test.ts
+++ src/tests/game.test.ts
@@ -5,3 +5,15 @@ test('computer places all ships', () => {
     expect(Computer.board.shipsLeft).toBe(5);
 });
 
+test('computer can attack player', () => {
+    Computer.cpuAttack();
+    expect(Human.board.board.filter((index: string) => index != '')).toEqual(['miss'])
+});
+
+test('computer trys attacking until an attack lands', () => {
+    for(let i = 1; i < Human.board.board.length; i++) {
+        Human.board.board[i] = 'miss';
+    }
+    Computer.cpuAttack();
+    expect(Human.board.board.filter((index: string) => index != 'miss')).toEqual([]);
+});
